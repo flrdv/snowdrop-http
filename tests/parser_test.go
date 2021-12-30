7ecbdcaad6725608aee8ca71c387181163f86831
floordiv <pavloromanenko25@gmail.com>
2021-12-30 19:05:57 +0200

    tests/parser_test.go: updated tests, now it looks a bit better

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -1,17 +1,20 @@
 package httpparser
 
 import (
+	"fmt"
 	"github.com/floordiv/snowdrop/src/httpparser"
+	"strings"
 	"testing"
 )
 
 
 type Protocol struct {
-	Method []byte
-	Path []byte
-	Protocol []byte
-	Headers map[string]string
-	Body []byte
+	Method 		[]byte
+	Path 		[]byte
+	Protocol 	[]byte
+	Headers 	map[string]string
+	Body 		[]byte
+	Completed 	bool
 }
 
 func (p *Protocol) OnMessageBegin() {}
@@ -42,7 +45,59 @@ func (p *Protocol) OnBody(chunk []byte) {
 	p.Body = append(p.Body, chunk...)
 }
 
-func (p *Protocol) OnMessageComplete() {}
+func (p *Protocol) OnMessageComplete() {
+	p.Completed = true
+}
+
+
+func expect(
+	protocol Protocol,
+	expectedMethod, expectedPath, expectedProtocolString string,
+	headers map[string]string,
+	expectedBodyLength int,
+	strictHeadersCheck bool) (succeeded bool, err string) {
+
+	if string(protocol.Method) != expectedMethod {
+		return false, fmt.Sprintf("expected method %s, got %s instead",
+			string(protocol.Method), expectedMethod)
+	}
+	if string(protocol.Path) != expectedPath {
+		return false, fmt.Sprintf("expected path %s, got %s instead",
+			string(protocol.Path), expectedPath)
+	}
+	if string(protocol.Protocol) != expectedProtocolString {
+		return false, fmt.Sprintf("expected protocol %s, got %s instead",
+			string(protocol.Protocol), expectedProtocolString)
+	}
+
+	for key, value := range headers {
+		if !strictHeadersCheck { key = strings.ToLower(key) }
+
+		expectedValue, found := headers[key]
+
+		if !strictHeadersCheck { expectedValue = strings.ToLower(expectedValue) }
+
+		if !found {
+			if strictHeadersCheck {
+				return false, fmt.Sprintf("unexpected header: %s", key)
+			} else {
+				continue
+			}
+		}
+
+		if expectedValue != value {
+			return false, fmt.Sprintf("%s: values are mismatching (expected %s, got %s)",
+				key, expectedValue, value)
+		}
+	}
+
+	if len(protocol.Body) != expectedBodyLength {
+		return false, fmt.Sprintf("mismatching body length: expected %d, got %d",
+			expectedBodyLength, len(protocol.Body))
+	}
+
+	return true, ""
+}
 
 
 func TestOrdinaryGETRequestParse(t *testing.T) {
@@ -52,8 +107,10 @@ func TestOrdinaryGETRequestParse(t *testing.T) {
 	methodExpected := "GET"
 	pathExpected := "/"
 	protocolExpected := "HTTP/1.1"
-	contentTypeHeaderExpected := "some content type"
-	hostHeaderExpected := "rush.dev"
+	headersExpected := map[string]string {
+		"Content-Type": "some content type",
+		"Host": "rush.dev",
+	}
 	bodyLenExpected := 0
 
 	ordinaryGetRequest := []byte("GET / HTTP/1.1\r\nContent-Type: some content type\r\nHost: rush.dev\r\n\r\n")
@@ -61,27 +118,16 @@ func TestOrdinaryGETRequestParse(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("parser returned error: %s\n", err)
+	} else if !protocol.Completed {
+		t.Error("the whole request was fed to parser but he does not think so")
 	}
 
-	if string(protocol.Method) != methodExpected {
-		t.Errorf("parsed method is invalid: expected %s, got %s", methodExpected, string(protocol.Method))
-	}
-	if string(protocol.Path) != pathExpected {
-		t.Errorf("parsed path is invalid: expected %s, got %s", pathExpected, string(protocol.Path))
-	}
-	if string(protocol.Protocol) != protocolExpected {
-		t.Errorf("parsed protocol is invalid: expected %s, got %s", protocolExpected, string(protocol.Protocol))
-	}
-	if protocol.Headers["Content-Type"] != contentTypeHeaderExpected {
-		t.Errorf("parsed Content-Type header is invalid: expected %s, got %s",
-			contentTypeHeaderExpected, protocol.Headers["Content-Type"])
-	}
-	if protocol.Headers["Host"] != hostHeaderExpected {
-		t.Errorf("parsed Host header is invalid: expected %s, got %s",
-			hostHeaderExpected, protocol.Headers["Host"])
-	}
-	if len(protocol.Body) != bodyLenExpected {
-		t.Errorf("parsed body is invalid: expected nothing, got \"%s\"", string(protocol.Body))
+	succeeded, errmsg := expect(protocol,
+		methodExpected, pathExpected, protocolExpected,
+		headersExpected, bodyLenExpected, true)
+
+	if !succeeded {
+		t.Error(errmsg)
 	}
 }
 
@@ -94,39 +140,67 @@ func TestOrdinaryPOSTRequestParse(t *testing.T) {
 	methodExpected := "POST"
 	pathExpected := "/"
 	protocolExpected := "HTTP/1.1"
-	contentTypeHeaderExpected := "some content type"
-	hostHeaderExpected := "rush.dev"
-	contentLengthHeaderExpected := "13"
+	headersExpected := map[string]string {
+		"Content-Type": "some content type",
+		"Host": "rush.dev",
+		"Content-Length": "13",
+	}
 	bodyLenExpected := 13
 
 	err := FeedParser(parser, ordinaryGetRequest, 5)
 
 	if err != nil {
 		t.Errorf("parser returned error: %s\n", err)
+	} else if !protocol.Completed {
+		t.Error("the whole request was fed to parser but he does not think so")
 	}
 
-	if string(protocol.Method) != methodExpected {
-		t.Errorf("parsed method is invalid: expected %s, got %s", methodExpected, string(protocol.Method))
+	succeeded, errmsg := expect(protocol,
+		methodExpected, pathExpected, protocolExpected,
+		headersExpected, bodyLenExpected, true)
+
+	if !succeeded {
+		t.Error(errmsg)
 	}
-	if string(protocol.Path) != pathExpected {
-		t.Errorf("parsed path is invalid: expected %s, got %s", pathExpected, string(protocol.Path))
+}
+
+func TestChromeGETRequest(t *testing.T) {
+	request := "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0" +
+		"\r\nsec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"" +
+		"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: \"Windows\"\r\nUpgrade-Insecure-Requests: 1" +
+		"\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96" +
+		".0.4664.110 Safari/537.36\r\n" +
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8," +
+		"application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate" +
+		"\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br" +
+		"\r\nAccept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6\r\nCookie: csrftoken=y1y3SinAMbiYy7yn9Oc" +
+		"blqbu0Ksvtdr7q0iEns09m1D6MWlZv8JjB472GWnwfuDG; Goland-1dc491b=e03b2736-65ce-4ad0-b7ab-e4f8e1715c8b\r\n\r\n"
+
+	protocol := Protocol{}
+	parser := httpparser.NewHTTPRequestParser(&protocol)
+	completed, err := parser.Feed([]byte(request))
+
+	methodExpected := "GET"
+	pathExpected := "/"
+	protocolExpected := "HTTP/1.1"
+	headersExpected := map[string]string {
+		"Host": "localhost:8080",
+		"Content-Type": "some content type",
+		"Accept-Encoding": "gzip, deflate, br",
 	}
-	if string(protocol.Protocol) != protocolExpected {
-		t.Errorf("parsed protocol is invalid: expected %s, got %s", protocolExpected, string(protocol.Protocol))
+	bodyLenExpected := 0
+
+	if err != nil {
+		t.Errorf("error while parsing: %s", err)
+	} else if !completed {
+		t.Errorf("the whole request was fed to parser but he does not think so")
 	}
-	if protocol.Headers["Content-Type"] != contentTypeHeaderExpected {
-		t.Errorf("parsed Content-Type header is invalid: expected %s, got %s",
-			contentTypeHeaderExpected, protocol.Headers["Content-Type"])
-	}
-	if protocol.Headers["Host"] != hostHeaderExpected {
-		t.Errorf("parsed Host header is invalid: expected %s, got %s",
-			hostHeaderExpected, protocol.Headers["Host"])
-	}
-	if protocol.Headers["Content-Length"] != contentLengthHeaderExpected {
-		t.Errorf("parsed Content-Length header is invalid: expected %s, got %s",
-			hostHeaderExpected, protocol.Headers["Content-Length"])
-	}
-	if len(protocol.Body) != bodyLenExpected {
-		t.Errorf("parsed body is invalid: expected nothing, got \"%s\"", string(protocol.Body))
+
+	succeeded, errmsg := expect(protocol,
+		methodExpected, pathExpected, protocolExpected,
+		headersExpected, bodyLenExpected, false)
+
+	if !succeeded {
+		t.Error(errmsg)
 	}
 }
