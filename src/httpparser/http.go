446913e0fe40dcc4cf2e4a7cef24be768d192a8e
floordiv <pavloromanenko25@gmail.com>
2022-01-01 18:58:48 +0200

    src/httpparser/http: added `IsMethodValid(method []byte)` method

--- src/httpparser/http.go
+++ src/httpparser/http.go
@@ -1,5 +1,6 @@
 package httpparser
 
+import "bytes"
 
 type HTTPMethod []byte
 
@@ -14,3 +15,21 @@ var (
 	TRACE HTTPMethod = []byte("TRACE")
 	PATCH HTTPMethod = []byte("PATCH")
 )
+
+func IsMethodValid(method []byte) bool {
+	eq := bytes.Equal
+
+	switch {
+	case eq(method, GET): 		return true
+	case eq(method, HEAD): 		return true
+	case eq(method, POST): 		return true
+	case eq(method, PUT):		return true
+	case eq(method, DELETE):	return true
+	case eq(method, CONNECT):	return true
+	case eq(method, OPTIONS):	return true
+	case eq(method, TRACE):		return true
+	case eq(method, PATCH): 	return true
+	}
+
+	return false
+}
