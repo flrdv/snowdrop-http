af4fa8faae9c0463a27c39db9125bbc2020d079c
floordiv <pavloromanenko25@gmail.com>
2022-01-01 21:18:03 +0200

    src/httpparser/errors.go: added error for exceeding buffer length (uncommon error), renamed NoSplitterWasFound to InvalidHeader as this error relies only to error while parsing header

--- src/httpparser/errors.go
+++ src/httpparser/errors.go
@@ -4,10 +4,11 @@ import "errors"
 
 var (
 	RequestSyntaxError 			= errors.New("request syntax error")
- 	NoSplitterWasFound 			= errors.New("no splitter was found")
+ 	InvalidHeader	 			= errors.New("invalid header line")
 	TooBigChunk 				= errors.New("chunk overflow")
 	TooBigChunkSize 			= errors.New("chunk size is too big")
 	NotEnoughChunk 				= errors.New("received unexpected CRLF before the whole chunk was received")
+	BufferSizeExceeded 			= errors.New("buffer's size is exceeded (anomaly)")
 
 	InvalidRequestData			= errors.New("request is invalid")
 	InvalidContentLengthValue 	= errors.New("invalid value for content-length header")
