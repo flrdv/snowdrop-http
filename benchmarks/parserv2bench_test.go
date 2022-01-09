f157450f138eec5c58f2d3cb49073d1fe3121b92
fakefloordiv <real.floordiv@gmail.com>
2022-01-09 14:50:50 +0200

    parserv2bench_test.go: added benchmarks for big chrome request

--- benchmarks/parserv2bench_test.go
+++ benchmarks/parserv2bench_test.go
@@ -6,20 +6,19 @@ import (
 	"github.com/floordiv/snowdrop/src/snowdrop"
 )
 
-
 type ProtocolV2 struct {}
 
 func (p *ProtocolV2) OnMessageBegin() {}
 
-func (p *ProtocolV2) OnMethod(_ []byte) {}
+func (p *ProtocolV2) OnMethod(_ string) {}
 
-func (p *ProtocolV2) OnPath(_ []byte) {}
+func (p *ProtocolV2) OnPath(_ string) {}
 
-func (p *ProtocolV2) OnProtocol(_ []byte) {}
+func (p *ProtocolV2) OnProtocol(_ string) {}
 
 func (p *ProtocolV2) OnHeadersBegin() {}
 
-func (p *ProtocolV2) OnHeader(_, _ []byte) {}
+func (p *ProtocolV2) OnHeader(_, _ string) {}
 
 func (p *ProtocolV2) OnHeadersComplete() {}
 
@@ -27,7 +26,7 @@ func (p *ProtocolV2) OnBody(_ []byte) {}
 
 func (p *ProtocolV2) OnMessageComplete() {}
 
-var request = []byte("GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0" +
+var bigChromeRequest = []byte("GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0" +
 	"\r\nsec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"" +
 	"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: \"Windows\"\r\nUpgrade-Insecure-Requests: 1" +
 	"\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96" +
@@ -36,12 +35,13 @@ var request = []byte("GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep
 	"application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate" +
 	"\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br" +
 	"\r\nAccept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6\r\nCookie: csrftoken=y1y3SinAMbiYy7yn9Oc" +
-	"blqbu0Ksvtdr7q0iEns09m1D6MWlZv8JjB472GWnwfuDG; Goland-1dc491b=e03b2736-65ce-4ad0-b7ab-e4f8e1715c8b\r\n\r\n")
+	"blqbu0Ksvtdr7qrwgwgwwefdewf8JjB472GWnwfuDG; Goland-1dc491b=gegwfewfewfewf4ad0-b7ab-e4f8e1715c8b\r\n\r\n")
+var smallGetRequest = []byte("GET / HTTP/1.1\r\nContent-Type: some content type\r\nHost: rush.dev\r\n\r\n")
 
 func BenchmarkSmallGETRequestBy1Char(b *testing.B) {
 	protocol := ProtocolV2{}
 	parser := snowdrop.NewHTTPRequestParser(&protocol, 65535)
-	chars := divideBytes(request, 1)
+	chars := divideBytes(smallGetRequest, 1)
 
 	b.ResetTimer()
 
@@ -55,7 +55,7 @@ func BenchmarkSmallGETRequestBy1Char(b *testing.B) {
 func BenchmarkSmallGETRequestBy5Chars(b *testing.B) {
 	protocol := ProtocolV2{}
 	parser := snowdrop.NewHTTPRequestParser(&protocol, 65535)
-	chars := divideBytes(request, 5)
+	chars := divideBytes(smallGetRequest, 5)
 
 	b.ResetTimer()
 
@@ -73,6 +73,45 @@ func BenchmarkSmallGETRequestFull(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		parser.Feed(request)
+		parser.Feed(smallGetRequest)
+	}
+}
+
+func BenchmarkBigChromeRequestBy1Char(b *testing.B) {
+	protocol := ProtocolV2{}
+	parser := snowdrop.NewHTTPRequestParser(&protocol, 65535)
+	chars := divideBytes(bigChromeRequest, 1)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, charsArr := range chars {
+			parser.Feed(charsArr)
+		}
+	}
+}
+
+func BenchmarkBigChromeRequestBy5Chars(b *testing.B) {
+	protocol := ProtocolV2{}
+	parser := snowdrop.NewHTTPRequestParser(&protocol, 65535)
+	chars := divideBytes(bigChromeRequest, 5)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, charsArr := range chars {
+			parser.Feed(charsArr)
+		}
+	}
+}
+
+func BenchmarkBigChromeRequestFull(b *testing.B) {
+	protocol := ProtocolV2{}
+	parser := snowdrop.NewHTTPRequestParser(&protocol, 65535)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		parser.Feed(bigChromeRequest)
 	}
 }
