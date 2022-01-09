d707b02a0162bbfab03adfc00dd94deee556e2d0
fakefloordiv <real.floordiv@gmail.com>
2022-01-09 15:09:46 +0200

    src/snowdrop/chunked.go: a chunked requests parser, old one, will be refactored soon

--- src/snowdrop/chunked.go
+++ src/snowdrop/chunked.go
@@ -2,7 +2,6 @@ package snowdrop
 
 import (
 	"bytes"
-	"fmt"
 	"strconv"
 )
 
@@ -17,7 +16,7 @@ type OnBodyCallback func([]byte)
 type chunkedBodyParser struct {
 	callback 					OnBodyCallback
 	state 						ChunkedBodyState
-	chunkLength  				uint16
+	chunkLength  				int
 	chunkBodyReceived 			int
 	buffer						[]byte
 	chunksReceived				int
@@ -40,47 +39,133 @@ func (p *chunkedBodyParser) Clear() {
 	p.state = ChunkLengthExpected
 	p.chunkLength = 0
 	p.chunkBodyReceived = 0
-	p.buffer = nil
+	p.buffer = p.buffer[:0]
 	p.chunksReceived = 0
 }
 
-func (p *chunkedBodyParser) Feed(data []byte) (done bool, err error) {
-	//fmt.Println("feeding with", quote(data))
-
+func (p *chunkedBodyParser) Feed(data []byte) (done bool, extraBytes []byte, err error) {
 	if p.state == BodyCompleted {
-		return true, nil
+		// it's not http parser, here we don't care about streaming parse
+		return true, nil, nil
 	}
-	if len(data) == 0 {
+	if len(data) == 0 || bytes.Equal(data, cr) {
+		return false, nil, nil
+	}
+
+	lfCount := bytes.Count(data, lf)
+
+	if lfCount == 0 {
+		done, err = p.parseStateDataPiece(bytes.TrimSuffix(data, cr))
+
+		return done, nil, err
+	} else {
+		nextLF := bytes.IndexByte(data, '\n')
+
+		for i := 0; i < lfCount; i++ {
+			piece := bytes.TrimPrefix(data[:nextLF], cr)
+			done, err = p.parseStateData(piece)
+
+			if err != nil {
+				return true, nil, err
+			}
+			if done {
+				return true, data[nextLF+1:], nil
+			}
+
+			data = data[nextLF+1:]
+			nextLF = bytes.IndexByte(data, '\n')
+		}
+
+		// this must be already the last one sliced element
+		done, err = p.parseStateDataPiece(data)
+
+		return done, nil, err
+	}
+}
+
+func (p *chunkedBodyParser) parseStateDataPiece(data []byte) (done bool, err error) {
+	switch p.state {
+	case ChunkExpected:
+		if p.chunkBodyReceived + len(data) > p.chunkLength {
+			p.state = BodyCompleted
+
+			return true, TooBigChunk
+		} else if p.chunkLength == 0 {
+			return true, nil
+		}
+
+		if p.pushBody(data) {
+			return true, nil
+		}
+	case ChunkLengthExpected:
+		if len(p.buffer)  > len(_maxHexChunkSize) {
+			p.state = BodyCompleted
+
+			return true, TooBigChunkSize
+		}
+
+		p.buffer = append(p.buffer, data...)
+
 		return false, nil
 	}
 
-	lines := bytes.Split(data, lf)
+	// this also shouldn't ever happen
+	// i hope
+	p.state = BodyCompleted
 
-	if len(p.buffer) > 0 {
-		lines[0] = append(p.buffer, lines[0]...)
-		p.buffer = nil
-	}
+	return true, AssertationError
+}
 
-	if len(lines) > 1 {
-		p.buffer = lines[len(lines)-1]
-		lines = lines[:len(lines)-1]
-	}
-
-	if len(lines[0]) == 0 {
-		lines = lines[1:]
-	}
-
-	for _, line := range lines {
-		//fmt.Println("chunk line:", quote(line))
-
-		p.state, err = p.parseDataAndGetNextStep(bytes.TrimSuffix(line, cr))
-
-		if p.state == BodyCompleted {
-			return true, err
+func (p *chunkedBodyParser) parseStateData(data []byte) (done bool, err error) {
+	switch p.state {
+	case ChunkExpected:
+		if p.chunkBodyReceived + len(data) > p.chunkLength {
+			// in this handler we're passing only whole pieces, so yep
+			return true, TooBigChunk
 		}
+
+		done = p.pushBody(data)
+
+		if !done {
+			p.chunkBodyReceived += len(data)
+		}
+
+		return false, nil
+	case ChunkLengthExpected:
+		rawChunkLength := B2S(p.buffer)
+		chunkLength, err := strconv.ParseInt(rawChunkLength, 16, 32)
+
+		if err != nil {
+			p.state = BodyCompleted
+
+			return true, InvalidChunkSize
+		}
+
+		p.chunkLength = int(chunkLength)
+
+		return false, nil
 	}
 
-	return false, nil
+	p.state = BodyCompleted
+
+	// this shouldn't ever happen
+	// i hope
+	return true, AssertationError
+}
+
+func (p *chunkedBodyParser) pushBody(data []byte) (done bool) {
+	p.callback(data)
+	p.chunkBodyReceived += len(data)
+
+	if p.chunkBodyReceived == p.chunkLength {
+		p.chunkBodyReceived = 0
+		p.chunksReceived++
+		p.state = ChunkLengthExpected
+
+		return true
+	}
+
+	return false
 }
 
 func (p *chunkedBodyParser) parseDataAndGetNextStep(data []byte) (nextState ChunkedBodyState, err error) {
@@ -98,19 +183,19 @@ func (p *chunkedBodyParser) parseDataAndGetNextStep(data []byte) (nextState Chun
 			return BodyCompleted, TooBigChunkSize
 		}
 
-		chunkLength, err := strconv.ParseUint(string(data), 16, 16)
+		chunkLength, err := strconv.ParseInt(string(data), 16, 32)
 
 		if err != nil {
-			fmt.Println("invalid chunk size:", quote(p.buffer), quote(data))
 			return BodyCompleted, InvalidChunkSize
+		} else if chunkLength > _maxChunkSize {
+			return BodyCompleted, TooBigChunkSize
 		}
 
-		p.chunkLength = uint16(chunkLength)
+		p.chunkLength = int(chunkLength)
 
 		return ChunkExpected, nil
 	case ChunkExpected:
-		if uint16(p.chunkBodyReceived + len(data)) > p.chunkLength {
-			fmt.Println("o:", p.chunkBodyReceived, p.chunkLength, quote(data))
+		if p.chunkBodyReceived + len(data) > p.chunkLength {
 			return BodyCompleted, TooBigChunk
 		}
 		if p.chunkLength == 0 {
@@ -120,7 +205,7 @@ func (p *chunkedBodyParser) parseDataAndGetNextStep(data []byte) (nextState Chun
 		p.callback(data)
 		p.chunkBodyReceived += len(data)
 
-		if uint16(p.chunkBodyReceived) == p.chunkLength {
+		if p.chunkBodyReceived == p.chunkLength {
 			p.chunkBodyReceived = 0
 			p.chunksReceived++
 
