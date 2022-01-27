60145c85b641fb742616756b1e27fe6635b8fd98
fakefloordiv <real.floordiv@gmail.com>
2022-01-28 00:37:01 +0200

    src/snowdrop/intparse.go: two functions, for parsing 10-numeral and hex integers; using them as std-lib variants are not enough for me cause of speed

---
+++ src/snowdrop/intparse.go
@@ -0,0 +1,41 @@
+package snowdrop
+
+func parseUint(raw []byte) (num int, err error) {
+	/*
+		Tiny implementation of strconv.Atoi, but using directly bytes array,
+		and returning only one error in case of shit - InvalidContentLength
+
+		Parses 10-numeral system integers
+	*/
+
+	for _, char := range raw {
+		char -= '0'
+
+		if char > 9 {
+			return 0, InvalidContentLength
+		}
+
+		num = num * 10 + int(char)
+	}
+
+	return num, nil
+}
+
+func parseHex(raw []byte) (num int, err error) {
+	/*
+		Tiny implementation of strconv.ParseUint(raw, 16, 0), but made custom for myself,
+		as I don't need all that stuff from strconv.ParseUint()
+
+		Input data MUST be in lower-case
+	*/
+
+	for _, char := range raw {
+		if (char < '0' && char > '9') && (char < 'a' && char > 'f') && (char < 'A' && char > 'F') {
+			return 0, InvalidChunkSize
+		}
+
+		num = (num << 4) + int((char & 0xF) + 9 * (char >> 6))
+	}
+
+	return num, nil
+}
