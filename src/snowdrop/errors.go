c3e508df27e050d0d31d09bd2db9318042620c9e
fakefloordiv <real.floordiv@gmail.com>
2022-01-28 00:34:47 +0200

    src/snowdrop/errors.go: added error for invalid body splitter

--- src/snowdrop/errors.go
+++ src/snowdrop/errors.go
@@ -7,11 +7,11 @@ var (
 	InvalidHeader	 			= errors.New("invalid header line")
 	BufferSizeExceeded 			= errors.New("buffer's size is exceeded (anomaly)")
 	InvalidRequestData			= errors.New("request is invalid")
-	InvalidContentLengthValue 	= errors.New("invalid value for content-length header")
+	InvalidContentLength    	= errors.New("invalid value for content-length header")
 
-	TooBigChunk 				= errors.New("chunk overflow")
 	TooBigChunkSize 			= errors.New("chunk size is too big")
 	InvalidChunkSize			= errors.New("chunk size is invalid hexdecimal value")
+	InvalidChunkSplitter		= errors.New("invalid splitter")
 
 	AssertationError			= errors.New("BUG")
 	ParserIsDead 				= errors.New("once error occurred, parser cannot be used anymore")
