5fc5acd6532e4f2359e0581bb7fa444dd9731779
fakefloordiv <real.floordiv@gmail.com>
2022-02-05 01:45:55 +0200

    src/httpparser/methods.go: the only thing changed is formatting

--- src/httpparser/methods.go
+++ src/httpparser/methods.go
@@ -1,19 +1,19 @@
-package snowdrop
+package httpparser
 
 import "bytes"
 
 type HTTPMethod []byte
 
 var (
-	GET HTTPMethod = []byte("GET")
-	HEAD HTTPMethod = []byte("HEAD")
-	POST HTTPMethod = []byte("POST")
-	PUT HTTPMethod = []byte("PUT")
-	DELETE HTTPMethod = []byte("DELETE")
+	GET     HTTPMethod = []byte("GET")
+	HEAD    HTTPMethod = []byte("HEAD")
+	POST    HTTPMethod = []byte("POST")
+	PUT     HTTPMethod = []byte("PUT")
+	DELETE  HTTPMethod = []byte("DELETE")
 	CONNECT HTTPMethod = []byte("CONNECT")
 	OPTIONS HTTPMethod = []byte("OPTIONS")
-	TRACE HTTPMethod = []byte("TRACE")
-	PATCH HTTPMethod = []byte("PATCH")
+	TRACE   HTTPMethod = []byte("TRACE")
+	PATCH   HTTPMethod = []byte("PATCH")
 )
 
 var HTTPMethods = [][]byte{GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH}
