281e00bd84d394b135db8e8d9316d9e901a5b43c
fakefloordiv <real.floordiv@gmail.com>
2022-02-08 14:56:52 +0200

    src/httpparser/errors.go: added error name into error text instead of plane describe

--- src/httpparser/errors.go
+++ src/httpparser/errors.go
@@ -3,19 +3,19 @@ package httpparser
 import "errors"
 
 var (
-	ErrInvalidMethod        = errors.New("invalid method")
-	ErrInvalidPath          = errors.New("path is empty or contains disallowed characters")
-	ErrProtocolNotSupported = errors.New("protocol is not supported")
-	ErrInvalidHeader        = errors.New("invalid header line")
-	ErrBufferOverflow       = errors.New("buffer overflow")
-	ErrInvalidContentLength = errors.New("invalid value for content-length header")
-	ErrRequestSyntaxError   = errors.New("request syntax error")
-	ErrBodyTooBig           = errors.New("received too much body before connection closed")
+	ErrInvalidMethod        = errors.New("ErrInvalidMethod: invalid method")
+	ErrInvalidPath          = errors.New("ErrInvalidPath: path is empty or contains disallowed characters")
+	ErrProtocolNotSupported = errors.New("ErrProtocolNotSupported: protocol is not supported")
+	ErrInvalidHeader        = errors.New("ErrInvalidHeader: invalid header line")
+	ErrBufferOverflow       = errors.New("ErrBufferOverflow: buffer overflow")
+	ErrInvalidContentLength = errors.New("ErrInvalidContentLength: invalid value for content-length header")
+	ErrRequestSyntaxError   = errors.New("ErrRequestSyntaxError: request syntax error")
+	ErrBodyTooBig           = errors.New("ErrBodyTooBig: received too much body before connection closed")
 
-	ErrTooBigChunkSize      = errors.New("chunk size is too big")
-	ErrInvalidChunkSize     = errors.New("chunk size is invalid hexdecimal value")
-	ErrInvalidChunkSplitter = errors.New("invalid splitter")
+	ErrTooBigChunkSize      = errors.New("ErrTooBigChunkSize: chunk size is too big")
+	ErrInvalidChunkSize     = errors.New("ErrInvalidChunkSize: chunk size is invalid hexdecimal value")
+	ErrInvalidChunkSplitter = errors.New("ErrInvalidChunkSplitter: invalid splitter")
 
-	ErrConnectionClosed = errors.New("connection is closed, body has been received")
-	ErrParserIsDead     = errors.New("once error occurred, parser cannot be used anymore")
+	ErrConnectionClosed = errors.New("ErrConnectionClosed: connection is closed, body has been received")
+	ErrParserIsDead     = errors.New("ErrParserIsDead: once error occurred, parser cannot be used anymore")
 )
